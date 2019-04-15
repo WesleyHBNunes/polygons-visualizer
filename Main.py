b4b069c52de515cbd79934d05535ead2e0cdc462
wesley <wesley.rn2@gmail.com>
2019-04-15 16:02:01 -0300

    Change name of function polygons_from_file to polygons_from_file_xls and added a new test.

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
-import Visualizer
 import random
+
 import numpy as np
 from pyexcel_xls import get_data
+
+import Visualizer
 
 # CONSTS
 COLLUM_LINE_WIDTH_DATA = 3
@@ -17,7 +19,7 @@ def random_polygons(n, title):
     Visualizer.plot_polygons(array_polygons, title)
 
 
-def polygons_from_file(file_name, sheet, title):
+def polygons_from_file_xls(file_name, sheet, title):
     data = get_data(file_name)
     x_lim = data[sheet][COLLUM_LINE_WIDTH_DATA][COLLUM_LINE_WIDTH_DATA]
     y_lim = data[sheet][COLLUM_LINE_WIDTH_DATA][COLLUM_LINE_WIDTH_DATA]
@@ -30,7 +32,7 @@ def polygons_from_file(file_name, sheet, title):
                 j = j + 1
                 while j < len(data[sheet][i]):
                     array_points_x.append(data[sheet][i][j])
-                    array_points_y.append(data[sheet][i+1][j])
+                    array_points_y.append(data[sheet][i + 1][j])
                     j = j + 1
                 i = i + 1
                 array_points_tuple = list(zip(array_points_x, array_points_y))
@@ -42,8 +44,9 @@ def polygons_from_file(file_name, sheet, title):
 
 def main():
     # random_polygons(5, "Test of random polygons")
-    polygons_from_file("Test/dighe.xls", "Dighe1", "Test of instance Dighe.xls")
-    # polygons_from_file("Test/albano.xls", "Albano", "Test of instance Albano.xls")
+    polygons_from_file_xls("Test/dighe.xls", "Dighe1", "Test of instance Dighe.xls")
+    # polygons_from_file_xls("Test/dighe.xls", "Dighe2", "Test of instance Dighe.xls")
+    # polygons_from_file_xls("Test/albano.xls", "Albano", "Test of instance Albano.xls")
 
 
 if __name__ == "__main__":
