ede2ee812c0e5e2b472f7361949113f13a35f8a2
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-04-30 19:37:33 -0300

    Update function plot_polygons

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ from Visualizer import Visualizer
 def main():
     polygons, limits = File.polygons_from_xls("Test/han.xls", "Han")
     visualizer = Visualizer(polygons, limits[0], limits[1], "Title animation plot")
-    visualizer.plot_polygons()
-    # visualizer.plot_animation()
+    # visualizer.plot_polygons()
+    visualizer.plot_animation()
 
 
 if __name__ == "__main__":
